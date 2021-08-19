df3777dc24bffc9a50a715b72b3b9bd6dc1eb97c
sturzael <esturz23@gmail.com>
2021-08-19 16:23:16 +1200

    install morgan

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import express from 'express';
 import dotenv = require('dotenv');
 
-//load env variables
-dotenv.config({ path: './config/config.env' });
-
-const app = express();
-const port = process.env.PORT || 5000;
+//Packages
+import morgan = require('morgan');
 
 //Routes
 import chores = require('./routes/chores');
+
+//load env variables
+dotenv.config({ path: './config/config.env' });
+
+//set express && port
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(morgan('dev'));
 
 //Mount Router
 app.use('/api/v1/chores', chores);
